f764c1e944229d6afc5fce368b5b0ef69d0e05c0
Erlend <erlendk.kodehode@gmail.com>
2024-12-12 15:03:10 +0100

    finishing touches and commets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import styled, { keyframes } from "styled-components";
-import CardInfo from "./Card.jsx";
+import CardContents from "./Card.jsx";
 import { kidsWishlist } from "./wishlist.jsx";
 import decoration from "/christmas-decoration-clipart.svg";
 import spark from "/Star.png";
-//TODO MEDIA QUERIES
+import hat from "/santa-hat-icon-original.svg";
+
+//array of images to use as background images on the cards
+const stamps = [
+  "christmas-ball-silhouette.svg",
+  "christmas-bell-silhouette.svg",
+  "christmas-sock-silhouette.svg",
+  "christmas-tree-silhouette.svg",
+  "snowman-silhouette.svg",
+];
+
 const Main = styled.main`
   display: flex;
   flex-direction: column;
@@ -13,15 +23,29 @@ const Main = styled.main`
 `;
 
 const Title = styled.h1`
+  position: relative;
   font-family: "Mountains of Christmas", serif;
   color: #fef4e6;
   font-size: 4em;
   @media only screen and (max-width: 650px) {
+    font-size: 3em;
+  }
+  @media only screen and (max-width: 500px) {
     font-size: 2em;
   }
-  @media only screen and (max-width: 350px) {
+  @media only screen and (max-width: 340px) {
     font-size: 1.5em;
   }
+`;
+
+const HatImg = styled.img`
+  width: 0.5em;
+  position: absolute;
+  top: 6%;
+  left: -2.5%;
+
+  //flips and rotates the image
+  transform: scaleX(-1) rotate(45deg);
 `;
 
 const Grid = styled.div`
@@ -33,75 +57,46 @@ const Grid = styled.div`
   @media only screen and (max-width: 1165px) {
     grid-template-columns: repeat(3, 33%);
   }
-
   @media only screen and (max-width: 950px) {
     grid-template-columns: repeat(2, 50%);
     width: 70%;
   }
-
   @media only screen and (max-width: 720px) {
     width: 85%;
   }
-
   @media only screen and (max-width: 600px) {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
 `;
+
 const CardBackground = styled.div`
   border-radius: 1rem;
   padding: 0.7rem;
-  background-image: url("/Card_Background.png");
+  background-image: url("/Card_Background.svg");
   position: relative;
 `;
 
 const DecorationImg = styled.img`
   position: absolute;
   top: -1em;
+
+  //centers the image
   right: 50%;
   transform: translate(50%);
+
   height: 18%;
 `;
 
-const Footer = styled.footer`
-  width: 100vw;
-  height: 20%;
-  background-color: crimson;
-`;
-
-const SparkAnim = keyframes`
-  0%{
-    transform: scale(0);
-  }
-
-  10%{
-    transform: scale(1);
-  }
-
-  15%{
-    transform: scale(0.9);
-  }
-
-  35%{
-    transform: scale(0);
-  }
- 
-`;
-
-const SparkImg = styled.img`
-  transform: scale(0);
-  animation: ${(props) => (props.$naughty ? "" : SparkAnim)} 1500ms linear
-    infinite;
-  height: 15%;
-  position: absolute;
-  top: -0.35em;
-  left: -0.15em;
-`;
-//TODO subtle background image
 const Card = styled.div`
+  //gets the background image from a property on the component so it can be randomized uniquely for each card
+  background: #f8f5ee url("/stamps/${(props) => props.$stamp}") no-repeat 86%
+    91%/15%;
+
+  //gives a special border to the good children
   border: 2px solid ${(props) => (props.$naughty ? "#f1e8d6" : "#f7e268")};
-  background-color: #f8f5ee;
+
   border-radius: 0.5rem;
   padding: 1.5em;
   display: flex;
@@ -116,6 +111,41 @@ const Card = styled.div`
   }
 `;
 
+const SparkImg = styled.img`
+  transform: scale(0) rotate(0);
+
+  //only animates if the child is good
+  animation: ${(props) => (props.$naughty ? "" : SparkAnim)} 1500ms linear
+    infinite;
+
+  height: 15%;
+  position: absolute;
+  top: -0.35em;
+  left: -0.15em;
+`;
+
+const SparkAnim = keyframes`
+  0%{
+    transform: scale(0) rotate(0);
+  }
+
+  10%{
+    transform: scale(1) rotate(90deg);
+  }
+
+  15%{
+    transform: scale(0.9) rotate(115deg);
+  }
+
+  34%{
+    transform: scale(0.1) rotate(180deg);
+  }
+  35%{
+    transform: scale(0) rotate(0);
+  }
+`;
+
+//figures out the length of the longest wishlist
 let longestWishlist = 0;
 kidsWishlist.forEach((kid) => {
   const listLength = kid.wishlist.length;
@@ -124,25 +154,28 @@ kidsWishlist.forEach((kid) => {
 
 export default function App() {
   return (
-    <>
-      <Main>
-        <Title>Øsnkelister for Julenissen</Title>
-        <Grid>
-          {kidsWishlist.map((kid, i) => (
-            <CardBackground key={i}>
-              <DecorationImg src={decoration} />
-              <Card $naughty={kid.naughty}>
-                <CardInfo
-                  data={kid}
-                  longestListLength={longestWishlist}
-                ></CardInfo>
-                <SparkImg src={spark} $naughty={kid.naughty} />
-              </Card>
-            </CardBackground>
-          ))}
-        </Grid>
-      </Main>
-      <Footer>Test</Footer>
-    </>
+    <Main>
+      <Title>
+        <HatImg src={hat} />
+        Øsnkelister for Julenissen
+      </Title>
+      <Grid>
+        {kidsWishlist.map((kid, i) => (
+          <CardBackground key={i}>
+            <DecorationImg src={decoration} />
+            <Card
+              $naughty={kid.naughty}
+              $stamp={stamps[Math.floor(Math.random() * stamps.length)]}
+            >
+              <CardContents
+                data={kid}
+                longestListLength={longestWishlist}
+              ></CardContents>
+              <SparkImg src={spark} $naughty={kid.naughty} />
+            </Card>
+          </CardBackground>
+        ))}
+      </Grid>
+    </Main>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -6,15 +6,6 @@ const NameAgeBox = styled.div`
   align-items: center;
   gap: 0.5rem;
 `;
-const WishlistBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  ul {
-    list-style-type: none;
-    font-size: 0.95em;
-  }
-`;
 
 const InfoBox = styled.div`
   display: flex;
@@ -29,26 +20,44 @@ const WishlistName = styled.h2`
   width: 5em;
 `;
 
+//uses same styling as WishlistName except for width
 const WishlistAge = styled(WishlistName)`
   width: 7em;
+`;
+
+const WishlistBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  //removes list styling and slighlty lowers the font size to make the text fit better
+  ul {
+    list-style-type: none;
+    font-size: 0.95em;
+  }
 `;
 
 const WishItem = styled.li`
   border-bottom: 1px solid black;
   font-family: "Schoolbell", cursive;
+  //gets padding from a property on the component so it can be randomized uniquely for each list item
+  //the padding is randomized to mimic a childs messy writing
   padding-left: ${(props) => props.offset}em;
   min-height: 1.5em;
 `;
 
-const minOffset = 0.3;
+//minimum and maximum that the randomizer uses
+const minOffset = 3;
 const maxOffset = 6;
 
-export default function CardInfo({
+export default function CardContents({
   data: { name, age, wishlist },
   longestListLength,
 }) {
+  //make all wishlist equally long by giving shorter wishlist empty arrayItems
+  //this is done to sell the illusion that this was a list that the children filled out
   const listLength = longestListLength - wishlist.length;
   for (let i = 0; i < listLength; i++) wishlist.push("");
+
   return (
     <>
       <NameAgeBox>
@@ -67,7 +76,7 @@ export default function CardInfo({
           {wishlist.map((item, i) => (
             <WishItem
               key={i}
-              offset={Math.floor(Math.random() * maxOffset) / 10 + minOffset}
+              offset={Math.floor(Math.random() * maxOffset + minOffset) / 10}
             >
               {item}
             </WishItem>
